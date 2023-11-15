155eebdf3f3eb5244b46ec0a84c8d72d240fe2e3
sophoan <sophoan.meas@hotmail.com>
2023-11-14 23:44:49 -0500

    add modal component

---
+++ components/ui/modal.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { 
+    Dialog, 
+    DialogContent, 
+    DialogDescription, 
+    DialogHeader, 
+    DialogTitle 
+} from "@/components/ui/dialog";
+
+interface ModalPros {
+    title: string;
+    description: string;
+    isOpen: boolean;
+    onClose: () => void;
+    children?: React.ReactNode;
+}
+
+export const Modal: React.FC<ModalPros> = ({
+    title,
+    description,
+    isOpen,
+    onClose,
+    children
+}) => {
+    const onChange = (open: boolean) => {
+        if (!open) {
+            onClose();
+        }
+    }
+
+    return (
+        <Dialog open={isOpen} onOpenChange={onChange}>
+            <DialogContent>
+                <DialogTitle>{title}</DialogTitle>
+                <DialogDescription>{description}</DialogDescription>
+                <DialogHeader>{children}</DialogHeader>
+            </DialogContent>
+        </Dialog>
+    )
+}
